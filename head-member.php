764e9daa1f549185880947fef94b2094529b8675
insu1005 <songinsu1005@gmail.com>
2022-12-23 12:29:50 +0900

    member-complete-but-need-to-fix-footer

--- head-member.php
+++ head-member.php
@@ -74,4 +74,3 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
 <!-- } 상단 끝 -->
 
 <!-- 콘텐츠 시작 { -->
-        <?php if (!defined("_INDEX_")) { ?><h2 id="container_title"><span title="<?php echo get_text($g5['title']); ?>"><?php echo get_head_title($g5['title']); ?></span></h2><?php }
