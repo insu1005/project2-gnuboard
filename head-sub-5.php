d7a20baf5bf0937725fb859d0253b19a31857c62
insu1005 <songinsu1005@gmail.com>
2022-12-27 10:39:44 +0900

    change

--- head-sub-5.php
+++ head-sub-5.php
@@ -105,4 +105,3 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
                    </ul>
             </div>
         </div>
-        <?php if (!defined("_INDEX_")) { ?><h2 id="container_title"><span title="<?php echo get_text($g5['title']); ?>"><?php echo get_head_title($g5['title']); ?></span></h2><?php }
