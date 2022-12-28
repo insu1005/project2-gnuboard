6be6aecb5e4b6f6681352bdfbf75d04ce98513ea
insu song <songinsu1005@gmail.com>
2022-12-29 01:56:39 +0900

    sub4-2-end

--- sub/sub4-2.php
+++ sub/sub4-2.php
@@ -26,17 +26,19 @@ include_once(G5_PATH.'/head-sub-4.php');
                             </div>
                         </div>
                         <div class="drama-box">
-                            <h4 class="drama-detail">오구오구! 여러분들과 함께하는 안방극장 오구쌀이 직접지원합니다. 많이 사랑해주세요. 항상 오구쌀피자는 여러분들과 함께하고 싶습니다.</h4>
-                            <figure class="k-drama snack">
-                                <img src="/img/sub-img/k-drama-secret.png" alt="오구쌀의 아낌없는 드라마 제작지원! 사랑의 꽈배기 오구쌀피자가 함께합니다!">
-                                <h5>사랑의 꽈배기</h5>
-                                <p>거짓말 때문에 사랑과 인생이 총체적으로 꼬여버린 막장가족들의 코믹 멜로 휴먼가족 이야기</p>
-                            </figure>
-                            <figure class="k-drama secret">
-                                <img src="/img/sub-img/k-drama-snack.png" alt="오구쌀의 아낌없는 드라마 제작지원! 여자의 비밀 오구쌀피자가 함께합니다!">
-                                <h5>여자의 비밀</h5>
-                                <p>거짓말 때문에 사랑과 인생이 총체적으로 꼬여버린 막장가족들의 코믹 멜로 휴먼가족 이야기</p>
-                            </figure>
+                            <div class="drama-fix clearfix">
+                                <figure class="k-drama snack">
+                                    <img src="/img/sub-img/k-drama-snack.png" alt="오구쌀의 아낌없는 드라마 제작지원! 사랑의 꽈배기 오구쌀피자가 함께합니다!">
+                                    <h5>사랑의 꽈배기</h5>
+                                    <p>거짓말 때문에 사랑과 인생이 총체적으로 꼬여버린<br>막장가족들의 코믹 멜로 휴먼가족 이야기</p>
+                                </figure>
+                                <figure class="k-drama secret">
+                                    <img src="/img/sub-img/k-drama-secret.png" alt="오구쌀의 아낌없는 드라마 제작지원! 여자의 비밀 오구쌀피자가 함께합니다!">
+                                    <h5>여자의 비밀</h5>
+                                    <p>아버지의 복수와 빼앗긴 아이를 되찾기 위해 새하얀 백조처럼 순수했던 여자가<br>흑조처럼 강인하게 변해가는 이야기를 그린 드라마</p>
+                                </figure>
+                            </div>
+                            <p class="drama-detail">오구오구! 여러분들과 함께하는 안방극장 오구쌀이 직접지원합니다. <br> 많이 사랑해주세요. 항상 오구쌀피자는 여러분들과 함께하고 싶습니다.</p>
                         </div>
                     </div>
                 </div>
@@ -47,14 +49,27 @@ include_once(G5_PATH.'/head-sub-4.php');
             <p class="sub-description">오구쌀만의 오구오구 브랜딩 홍보입니다</p>
             <div class="big-box-wrap">
                 <figure class="utube-branding clearfix">
-                    <!-- <div class="유튜브 여기에 삽입"></div> -->
-                    <h4 class="utube-tit">유튜브 브랜딩 영상</h4>
-                    <p class="utube-detail">유튜브 여러분 유하! 오구쌀피자가 유튜브에서도 여러분과 함께합니다</p>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/TFhrC2Dizv4" title="여러분과 함께하는 오구쌀피자!" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="utube-description">    
+                        <h4 class="utube-tit">유튜브 브랜딩 영상</h4>
+                        <div class="utube-detail">
+                            <p>유튜브 여러분 유하!</p> 
+                            <p>오구쌀피자가 유튜브에서도 여러분과 함께합니다</p>
+                    </div>
+                    </div>
                 </figure>
                 <figure class="utube-branding clearfix">
                     <!-- <div class="유튜브 여기에 삽입"></div> -->
-                    <h4 class="utube-tit">유튜브 컨텐츠</h4>
-                    <p class="utube-detail">많은 유튜버분들이 저희 오구쌀 피자와 함께합니다!</p>
+                    <div class="creator-box">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/zropnAaOVu0" title="YouTube video player" frameborder="1px solid #f00" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/8UcZYt22ark" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/LxdL2u8METU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/bezrLYSlW0M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                    <div class="creator-description">
+                        <h4 class="utube-tit">유튜브 컨텐츠</h4>
+                        <p class="utube-detail">많은 유튜버분들이 저희 오구쌀 피자와 함께합니다!</p>
+                    </div>
                 </figure>
             </div>
         </article>
@@ -62,26 +77,26 @@ include_once(G5_PATH.'/head-sub-4.php');
             <h3 class="sub-tit">Indoor 오구오구! Outdoor 오구오구!</h3>
             <p class="sub-description">언제어디서나 함께하는 오구쌀피자의 ALL-ROUND 마켓팅!</p>
             <div class="big-box-wrap">
-                <figure class="off-line-wrap">
-                    <img src="/img/sub-img/interior.png" alt="오프라인에서 만나요! 오구쌀과 함께하는 오프라인 타임!">
+                <figure class="off-line-wrap clearfix">
+                    <img src="/img/sub-img/offline-marketing.png" alt="오프라인에서 만나요! 오구쌀과 함께하는 오프라인 타임!">
                     <h4>오프라인 마켓팅</h4>
                     <div class="off-line-description">
-                        <p>여러분을 만나보고 같이 시간을 보내는 오구쌀입니다. 항상 열린마음으로 여러분에게 가고 싶습니다.</p>
-                        <p>때로는 친구같은 때로는 가족같은 때로는 애인같은 여러분에게 가까운 오구쌀이 되고 싶습니다.</p>
+                        <p>여러분을 만나보고 같이 시간을 보내는 오구쌀입니다. <br> 항상 열린마음으로 여러분에게 가고 싶습니다.</p>
+                        <p>때로는 친구같은 때로는 가족같은 때로는 애인같은 <br> 여러분에게 가까운 오구쌀이 되고 싶습니다.</p>
                         <p>언제 어디서든 참여 가능한 오구쌀만의 혜택! 지금 같이 함께해요! 오구오구! 여러분!</p>
                     </div>
                 </figure>
-                <figure class="radio-wrap">
-                    <img src="" alt="오구오구!! 지금 당장 청쥐해!! 오구쌀이 함께해요!">
+                <figure class="off-line-wrap clearfix">
+                    <img src="/img/sub-img/radio-marketing.png" alt="오구오구!! 지금 당장 청쥐해!! 오구쌀이 함께해요!">
                     <h4>라디오 광고</h4>
-                    <div class="radio-wrap-description">
+                    <div class="off-line-description">
                         <p>귀로 쏙 오구오구!</p>
                         <p>언제어디서 오구오구!</p>
                         <p>즐거운 오구오구!</p>
                     </div>
                 </figure>
                 <figure class="online-wrap">
-                    <img src="online-box" alt="온라인에서도 찾아볼 수 있어요! 오구오구 모바일, pc 인터넷 검색!! 바로지금!">
+                    <img src="/img/sub-img/online-marketing.png" alt="온라인에서도 찾아볼 수 있어요! 오구오구 모바일, pc 인터넷 검색!! 바로지금!" class="online-box">
                 </figure>
             </div>
         </article>
